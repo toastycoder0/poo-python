78b100a7013f2e4c5c82a0abe173389f02d4a18c
toastycoder0 <re-jos2002@proton.me>
2026-02-12 18:22:31 -0600

    feat: added ecuation show

--- ecuacion_formula_general.py
+++ ecuacion_formula_general.py
@@ -10,17 +10,18 @@ b = int(input("Ingresa el valor de b: "))
 c = int(input("Ingresa el valor de c: "))
 
 disc = pow(b, 2) - (4 * a * c)
-print("\nx² + bx + c = 0\n")
 if disc > 0:
     x1 = (-1 * b + sqrt(disc)) / (2 * a)
     x2 = (-1 * b - sqrt(disc)) / (2 * a)
     print("Raíces reales diferentes")
+    print(f"{a:.0f}x² + {b:.0f}x + {c:.0f} = 0")
     print(f"x1 es igual a {x1:.2f}")
     print(f"x2 es igual a {x2:.2f}")
 elif disc == 0:
     x1 = (-1 * b) / (2 * a)
     x2 = (-1 * b) / (2 * a)
     print("Raíces reales repetidas")
+    print(f"{a:.0f}x² + {b:.0f}x + {c:.0f} = 0")
     print(f"x1 es igual a {x1:.2f}")
     print(f"x2 es igual a {x2:.2f}")
 else:
