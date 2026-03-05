3af510eed0757e524c5f8f8a05e8b7b816275741
toastycoder0 <re-jos2002@proton.me>
2026-03-04 18:32:27 -0600

    feat: added new function

--- lista_de_palabras_doble.py
+++ lista_de_palabras_doble.py
@@ -14,17 +14,23 @@ def palabras_en_ambas():
     return palabras_en_ambas_listas
 
 
-def palabras_solo_en_uno():
-    pass
+def palabras_solo_en_lista_uno():
+    palabras_solo_en_lista_uno = []
+    for palabra in lista_uno:
+        if not palabra in lista_dos:
+            palabras_solo_en_lista_uno.append(palabra)
+    return palabras_solo_en_lista_uno
 
 
-def palabras_solo_en_dos():
+def palabras_solo_en_lista_dos():
     pass
 
 
 def principal():
     palabras = palabras_en_ambas()
-    print(palabras)
+    palabras_en_uno = palabras_solo_en_lista_uno()
+    print("Palabras en ambas: ", palabras)
+    print("Palabras en lista uno: ", palabras_en_uno)
 
 
 principal()
