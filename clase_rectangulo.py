0f57f67e6738c11e92bb4218ce185a347d9eadfb
toastycoder0 <re-jos2002@proton.me>
2026-03-12 18:33:59 -0600

    feat: added script change

--- clase_rectangulo.py
+++ clase_rectangulo.py
@@ -8,29 +8,34 @@ class Reactangulo:
         self.__largo = largo
         self.__ancho = ancho
 
-    def modificar_largo(self, nuevo_valor):
-        self.__largo = nuevo_valor
-
-    def modificar_ancho(self, nuevo_valor):
-        self.__ancho = nuevo_valor
-
-    def consultar_largo(self):
+    def consultarLargo(self):
         return self.__largo
 
-    def consultar_ancho(self):
+    def consultarAncho(self):
         return self.__ancho
 
-    def calcular_area(self):
+    def calcularArea(self):
         return self.__largo * self.__ancho
 
+    def modificarLargo(self, nuevo_valor):
+        self.__largo = nuevo_valor
 
-rectangulo = Reactangulo(2, 4)
+    def modificarAncho(self, nuevo_valor):
+        self.__ancho = nuevo_valor
 
-print(f"""
+
+rectangulo_uno = Reactangulo(2, 4)
+rectangulo_dos = Reactangulo(8, 6)
+
+rectangulos = [rectangulo_uno, rectangulo_dos]
+
+for rectangulo in rectangulos:
+    indice = rectangulos.index(rectangulo)
+    print(f"""
 ----------
-Rectangulo
+Rectangulo {indice + 1}
 ----------
-Largo:  {rectangulo.consultar_largo()}
-Ancho:  {rectangulo.consultar_ancho()}
-Area:   {rectangulo.calcular_area()}
-""")
+Largo:  {rectangulo.consultarLargo()}
+Ancho:  {rectangulo.consultarAncho()}
+Area:   {rectangulo.calcularArea()}
+    """)
