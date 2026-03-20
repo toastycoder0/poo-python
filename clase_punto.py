691454245f40c185b2770f35a9527516f0bc90b0
toastycoder0 <re-jos2002@proton.me>
2026-03-19 18:48:56 -0600

    feat: added new script

--- clase_punto.py
+++ clase_punto.py
@@ -1,24 +1,40 @@
+# pyright: reportRedeclaration=false
 import os
-from typing import Self
+from multimethod import multimethod
 
 
 class Punto2D:
-    def __init__(self, x, y):
+    @multimethod
+    def __init__(self, x: int, y: int):
         self.__x = x
         self.__y = y
+
+    @multimethod
+    def __init__(self, x: float, y: float):
+        self.__x = x
+        self.__y = y
+
+    @multimethod
+    def __init__(self):
+        self.__x = 0
+        self.__y = 0
 
     def __str__(self):
         return f"p({self.__x}, {self.__y})"
 
-    def obtener_x(self):
+    @property
+    def x(self):
         return self.__x
 
-    def obtener_y(self):
+    @property
+    def y(self):
         return self.__y
 
+    @x.setter
     def modificar_x(self, valor):
         self.__x = valor
 
+    @y.setter
     def modificar_y(self, valor):
         self.__y = valor
 
@@ -38,12 +54,16 @@ class Punto2D:
 
 os.system("clear")
 
-punto_uno = Punto2D(3, 5)
+punto_uno = Punto2D(2, 3)
 punto_dos = Punto2D(-3, 5)
 punto_tres = Punto2D(-3, -5)
 punto_cuatro = Punto2D(3, -5)
+punto_cinco = Punto2D()
+punto_seis = Punto2D(3.3, 4.4)
 
 print(punto_uno, "cuadrante", punto_uno.cuadrante())
 print(punto_dos, "cuadrante", punto_dos.cuadrante())
 print(punto_tres, "cuadrante", punto_tres.cuadrante())
 print(punto_cuatro, "cuadrante", punto_cuatro.cuadrante())
+print(punto_cinco, "cuadrante", punto_cinco.cuadrante())
+print(punto_seis, "cuadrante", punto_seis.cuadrante())
