4fb80fc71bdab4da4473a81144ccd28cb0b11b5f
toastycoder0 <re-jos2002@proton.me>
2026-03-24 18:21:17 -0600

    feat: added new code

--- clase_punto.py
+++ clase_punto.py
@@ -68,16 +68,37 @@ class Punto2D:
             return 0
         return None
 
+    def __add__(self, v2):
+        return Punto2D(self.x + v2.x, self.y + v2.y)
+
+    def __sub__(self, v2):
+        return Punto2D(self.x - v2.x, self.y - v2.y)
+
+    def __mul__(self, escalar):
+        return Punto2D(self.x * escalar, self.y * escalar)
+
+    #
+    # def __truediv__(self, n):
+    #     return Punto2D(self.x / n, self.y / n)
+
+    def __neg__(self):
+        return Punto2D(self.x * (-1), self.y * (-1))
+
 
 system("clear")
 
 punto_uno = Punto2D(2, 3)
 punto_dos = Punto2D(-3, 5)
 punto_tres = Punto2D(-3, -5)
+
 punto_cuatro = Punto2D(3, -5)
 punto_cinco = Punto2D()
 punto_seis = Punto2D(3.3, 4.4)
-punto_suma = punto_uno.suma(punto_dos)
+
+punto_suma = punto_uno + punto_dos
+punto_resta = punto_uno - punto_dos
+punto_multiplicacion = punto_uno * 2
+punto_negativo = -punto_uno
 
 print(punto_uno, "cuadrante", punto_uno.cuadrante())
 print(punto_dos, "cuadrante", punto_dos.cuadrante())
@@ -85,4 +106,12 @@ print(punto_tres, "cuadrante", punto_tres.cuadrante())
 print(punto_cuatro, "cuadrante", punto_cuatro.cuadrante())
 print(punto_cinco, "cuadrante", punto_cinco.cuadrante())
 print(punto_seis, "cuadrante", punto_seis.cuadrante())
-print(punto_suma, "cuadrante", punto_suma.cuadrante())
+print("Punto suma", punto_suma, "cuadrante", punto_suma.cuadrante())
+print("Punto resta", punto_resta, "cuadrante", punto_resta.cuadrante())
+print(
+    "Punto por escalar",
+    punto_multiplicacion,
+    "cuadrante",
+    punto_multiplicacion.cuadrante(),
+)
+print("Punto negativo", punto_negativo, "cuadrante", punto_negativo.cuadrante())
